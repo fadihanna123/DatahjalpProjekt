d6370c637ed08711a3016ca0bd8932250d208b02
Fadi Hanna <fadialias230@gmail.com>
2025-11-08 00:12:48 +0100

    Add form-validation, separate header, footer for all pages, remove unneeded css transition in most elements, make meta-description more generic and other improvments

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const pageLoadingEl = document.getElementById('pageLoading');
 const containerEl = document.getElementById('container');
 
-const initPage = () => {
+const initPage = async () => {
+  await Promise.all([
+    loadHTML('./inc/header.html', 'header-placeholder'),
+    loadHTML('./inc/footer.html', 'footer-placeholder'),
+  ]);
+
   setTimeout(() => {
     pageLoadingEl.style.display = 'none';
     containerEl.style.display = 'block';
-  }, 1000);
+  }, 500);
 };
 
 window.dataLayer = window.dataLayer || [];
@@ -15,3 +20,17 @@ function gtag() {
 gtag('js', new Date());
 
 gtag('config', 'G-TYYFG6B27M');
+
+// Function to load HTML into an element
+const loadHTML = async (file, elementId) => {
+  try {
+    const response = await fetch(file);
+    const html = await response.text();
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.innerHTML = html;
+    }
+  } catch (error) {
+    console.error(`Error loading ${file}:`, error);
+  }
+};
